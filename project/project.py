5f586015b54f1ee41207d4761c2a1c36c4dae6fb
2018135016 <ksj19103097@gmail.com>
2023-12-04 12:31:10 +0900

    foxxx

--- project/project.py
+++ project/project.py
@@ -1,9 +1,6 @@
-import matplotlib.pyplot as plt
-import pandas as pd
-import numpy as np
 import project_functions
 
-location = "C:\\Users\\pc\\Desktop\\컴퓨터프로그래밍\\프로젝트\\"
+location = ""
 
 project_functions.data_processing(location)
 project_functions.data_generation(location)
